4cadca1307dd96a0991cafd95ffaf6c067d43ada
devyonghee <devyonghee@gmail.com>
2022-10-22 13:46:53 +0900

    :art: add template method pattern

---
+++ template-method/src/main/java/me/devyonghee/templatemethod/Coffee.java
@@ -0,0 +1,12 @@
+package me.devyonghee.templatemethod;
+
+public class Coffee extends CaffeineBeverage {
+
+    void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
+}
---
+++ template-method/src/main/java/me/devyonghee/templatemethod/Tea.java
@@ -0,0 +1,12 @@
+package me.devyonghee.templatemethod;
+
+public class Tea extends CaffeineBeverage {
+
+    void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    void addCondiments() {
+        System.out.println("Adding Lemon");
+    }
+}
